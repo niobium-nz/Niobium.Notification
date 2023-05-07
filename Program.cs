7134467e067bf42288f19e73d28186b54cf62003
Wen Han <hcp5he11@gmail.com>
2023-05-08 00:48:14 +1200

    upgraded NuGet packages. added support to dependency injection.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
+    {
+        //s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+    })
     .Build();
 
 host.Run();
