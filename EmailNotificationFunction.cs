2687d4e6b441c9d6345a803a1db3e474cae8a54b
Wen Han <hcp5he11@gmail.com>
2023-05-12 21:09:57 +1200

    Added input data validation.

--- EmailNotificationFunction.cs
+++ EmailNotificationFunction.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
@@ -10,10 +12,35 @@ namespace Niobium.EmailNotification
         public EmailNotificationFunction(IEmailSender sender) => this.sender = sender;
 
         [Function(nameof(Notification))]
-        public async Task Notification([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, CancellationToken cancellationToken)
+        public async Task<HttpResponseData> Notification([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, CancellationToken cancellationToken)
         {
             var request = await req.ReadFromJsonAsync<NotificationRequest>(cancellationToken);
-            await this.sender.SendEmailAsync(request.Tenant, request.Message, request.Name, request.Contact);
+            if (request == null)
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var validationResults = new List<ValidationResult>();
+            var validates = Validator.TryValidateObject(request, new ValidationContext(request), validationResults, true);
+            if (!validates)
+            {
+                var response = req.CreateResponse();
+                await response.WriteAsJsonAsync(validationResults, cancellationToken);
+                response.StatusCode = HttpStatusCode.BadRequest;
+                return response;
+            }
+
+            ArgumentNullException.ThrowIfNull(request.ID);
+            ArgumentNullException.ThrowIfNull(request.Tenant);
+            ArgumentNullException.ThrowIfNull(request.Message);
+
+            var success = await this.sender.SendEmailAsync(request.Tenant, request.Message, request.Name, request.Contact);
+            if (!success)
+            {
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+
+            return req.CreateResponse(HttpStatusCode.Created);
         }
     }
 }
