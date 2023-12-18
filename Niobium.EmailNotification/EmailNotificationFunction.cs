f25ee3162323a4fffed2ea9d71597ea993c99b97
Wen Han <hcp5he11@gmail.com>
2023-12-18 22:46:46 +1300

    Tested function all working.

--- Niobium.EmailNotification/EmailNotificationFunction.cs
+++ Niobium.EmailNotification/EmailNotificationFunction.cs
@@ -1,71 +1,48 @@
 using System.ComponentModel.DataAnnotations;
 using System.Net;
-using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Niobium.EmailNotification
 {
-    public class EmailNotificationFunction
+    public class EmailNotificationFunction(IEmailSender sender, IVisitorRiskAssessor assessor)
     {
-        private readonly IEmailSender sender;
-        private readonly HtmlEncoder encoder;
-        private readonly IVisitorRiskAssessor assessor;
-
-        public EmailNotificationFunction(IEmailSender sender, HtmlEncoder encoder, IVisitorRiskAssessor assessor)
+        private static readonly JsonSerializerOptions options = new()
         {
-            this.sender = sender;
-            this.encoder = encoder;
-            this.assessor = assessor;
-        }
+            PropertyNameCaseInsensitive = true
+        };
 
         [Function(nameof(Notification))]
-        public async Task<HttpResponseData> Notification([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req, CancellationToken cancellationToken)
+        public async Task<IActionResult> Notification(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            CancellationToken cancellationToken)
         {
-            var request = await req.ReadFromJsonAsync<NotificationRequest>(cancellationToken);
-            if (request == null)
-            {
-                return req.CreateResponse(HttpStatusCode.BadRequest);
-            }
+            var request = await JsonSerializer.DeserializeAsync<NotificationRequest>(req.Body, options: options, cancellationToken: cancellationToken);
 
-            var badRequest = await req.ValidateAsync(request, cancellationToken);
-            if (badRequest != null)
-            {
-                return badRequest;
-            }
-
+            ArgumentNullException.ThrowIfNull(request);
             ArgumentNullException.ThrowIfNull(request.ID);
             ArgumentNullException.ThrowIfNull(request.Tenant);
             ArgumentNullException.ThrowIfNull(request.Message);
             ArgumentNullException.ThrowIfNull(request.Token);
 
+            var validationResults = new List<ValidationResult>();
+            var validates = Validator.TryValidateObject(request, new ValidationContext(request), validationResults, true);
+            if (!validates)
+            {
+                return new BadRequestObjectResult(validationResults);
+            }
+
             var lowRisk = await assessor.AssessAsync(request.Token, "contact-us", cancellationToken);
             if (!lowRisk)
             {
-                var response = req.CreateResponse();
-                var validationResults = new ValidationResult[]
-                {
-                    new ValidationResult("You are unable to prove that you are a human.", new string[]{ nameof(request.Token) }),
-                };
-                await response.WriteAsJsonAsync(validationResults, cancellationToken);
-                response.StatusCode = HttpStatusCode.BadRequest;
-                return response;
+                return new ForbidResult();
             }
 
-            request.Message = this.encoder.Encode(request.Message);
-
-            if (request.Name != null)
-            {
-                request.Name = this.encoder.Encode(request.Name);
-            }
-
-            if (request.Contact != null)
-            {
-                request.Contact = this.encoder.Encode(request.Contact);
-            }
-
-            var success = await this.sender.SendEmailAsync(request.Tenant, request.Message, request.Name, request.Contact, cancellationToken);
-            return !success ? req.CreateResponse(HttpStatusCode.InternalServerError) : req.CreateResponse(HttpStatusCode.Created);
+            var success = await sender.SendEmailAsync(request.Tenant, request.Message, request.Name, request.Contact, cancellationToken);
+            var statuscode = success ? HttpStatusCode.Created : HttpStatusCode.InternalServerError;
+            return new StatusCodeResult((int)statuscode);
         }
     }
 }
