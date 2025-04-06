587d68a2f9b2d5edee045753210d782b4bd3b62f
Wen Han <hcp5he11@gmail.com>
2025-04-06 23:37:44 +1200

    Completed greetings email function.

---
+++ Niobium.EmailNotification/Template.cs
@@ -0,0 +1,35 @@
+using Cod;
+
+namespace Niobium.EmailNotification
+{
+    public class Template
+    {
+        [EntityKey(EntityKeyKind.PartitionKey)]
+        public required string Tenant { get; set; }
+
+        [EntityKey(EntityKeyKind.RowKey)]
+        public required string Campaign { get; set; }
+
+        [EntityKey(EntityKeyKind.Timestamp)]
+        public DateTimeOffset? Timestamp { get; set; }
+
+        [EntityKey(EntityKeyKind.ETag)]
+        public string? ETag { get; set; }
+
+        public required string From { get; set; }
+
+        public required string Subject { get; set; }
+
+        public required string HTML { get; set; }
+
+        public static string BuildParitionKey(string tenant)
+        {
+            return tenant.Trim().ToLowerInvariant();
+        }
+
+        public static string BuildRowKey(string campaign)
+        {
+            return campaign.Trim();
+        }
+    }
+}
--- Niobium.EmailNotification/WelcomeFunction.cs
+++ Niobium.EmailNotification/WelcomeFunction.cs
@@ -1,16 +1,19 @@
-using System.Text.Json;
 using Azure.Messaging.ServiceBus;
+using Cod;
 using Cod.Messaging.ServiceBus;
+using Cod.Platform.Notification.Email;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 namespace Niobium.EmailNotification
 {
     public class WelcomeFunction(
+        IRepository<Template> repo,
+        IEmailNotificationClient sender,
         ILogger<WelcomeFunction> logger)
     {
         [Function(nameof(Welcome))]
-        public void Welcome(
+        public async Task Welcome(
             [ServiceBusTrigger("subscription", AutoCompleteMessages = true, Connection = nameof(ServiceBusOptions))]
             ServiceBusReceivedMessage message,
             CancellationToken cancellationToken)
@@ -21,7 +24,29 @@ namespace Niobium.EmailNotification
                 return;
             }
 
-            logger.LogInformation($"Welcome request: {JsonSerializer.Serialize(request)}");
+            var template = await repo.RetrieveAsync(
+                Template.BuildParitionKey(request.GetTenant()),
+                Template.BuildRowKey(request.GetCampaign()),
+                cancellationToken: cancellationToken);
+            ArgumentNullException.ThrowIfNull(template);
+
+            var body = template.HTML
+                .Replace("{{FIRST_NAME}}", request.FirstName)
+                .Replace("{{LAST_NAME}}", request.LastName)
+                .Replace("{{TENANT}}", request.GetTenant());
+
+            var success = await sender.SendAsync(
+                template.From,
+                [request.Email],
+                template.Subject,
+                body,
+                cancellationToken);
+            if (!success)
+            {
+                var error = $"Failed sending email to {template.HTML} for {request.GetCampaign()} by {request.GetTenant()}.";
+                logger.LogError(error);
+                throw new Cod.ApplicationException(InternalError.InternalServerError, internalMessage: error);
+            }
         }
     }
 }
