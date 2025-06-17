edad0f4c17ac1c5bc7dae9f21579c676845335dc
Wen Han <hcp5he11@gmail.com>
2025-06-18 01:05:27 +1200

    Upgraded dependencies.

--- Niobium.Notification.Core/WelcomeSubscriptionTrigger.cs
+++ Niobium.Notification.Core/WelcomeSubscriptionTrigger.cs
@@ -3,9 +3,9 @@ using Cod.Messaging;
 
 namespace Niobium.Notification
 {
-    internal class WelcomeSubscriptionTrigger(IMessagingBroker<Subscription> queue) : DomainEventHandler<SubscriptionDomain, EntityChangedEventArgs<Subscription>>
+    internal class WelcomeSubscriptionTrigger(IMessagingBroker<Subscription> queue) : DomainEventHandler<SubscriptionDomain, EntityChangedEvent<Subscription>>
     {
-        public async override Task HandleCoreAsync(EntityChangedEventArgs<Subscription> e, CancellationToken cancellationToken)
+        public async override Task HandleCoreAsync(EntityChangedEvent<Subscription> e, CancellationToken cancellationToken)
         {
             await queue.EnqueueAsync(new MessagingEntry<Subscription>
             {
