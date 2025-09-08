687c4b6b8ba6047fc3a39cd6d53439469004af90
Wen Han <hcp5he11@gmail.com>
2025-09-08 23:58:55 +1200

    Bugfix.

--- Niobium.Notification.Core/NotificationFlow.cs
+++ Niobium.Notification.Core/NotificationFlow.cs
@@ -10,28 +10,35 @@ namespace Niobium.Notification
     {
         public async Task RunAsync(NotifyCommand request, CancellationToken cancellationToken = default)
         {
-            var domain = await repo.GetAsync(
+            try
+            {
+                var domain = await repo.GetAsync(
                 Template.BuildParitionKey(request.Tenant),
                 Template.BuildRowKey(request.Channel),
                 cancellationToken: cancellationToken);
 
-            var deliverable = await domain.BuildAsync(request.Destination, request.Parameters, cancellationToken);
-            if (String.IsNullOrWhiteSpace(deliverable.Subject))
-            {
-                throw new ApplicationException(InternalError.InternalServerError, "Subject is required for email notification.");
-            }
+                var deliverable = await domain.BuildAsync(request.Destination, request.Parameters, cancellationToken);
+                if (String.IsNullOrWhiteSpace(deliverable.Subject))
+                {
+                    throw new ApplicationException(InternalError.InternalServerError, "Subject is required for email notification.");
+                }
 
-            var success = await sender.SendAsync(
-                new EmailAddress { Address = deliverable.From, DisplayName = deliverable.FromName },
-                [new EmailAddress { Address = deliverable.To, DisplayName = deliverable.ToName }],
-                deliverable.Subject,
-                deliverable.Body,
-                cancellationToken);
-            if (!success)
-            {
-                var error = $"Failed sending email to {deliverable.To} for {request.Channel} by {request.Channel}.";
-                logger.LogError(error);
-                throw new ApplicationException(InternalError.InternalServerError, internalMessage: error);
+                var success = await sender.SendAsync(
+                    new EmailAddress { Address = deliverable.From, DisplayName = deliverable.FromName },
+                    [new EmailAddress { Address = deliverable.To, DisplayName = deliverable.ToName }],
+                    deliverable.Subject,
+                    deliverable.Body,
+                    cancellationToken);
+                if (!success)
+                {
+                    var error = $"Failed sending email to {deliverable.To} for {request.Channel} by {request.Channel}.";
+                    logger.LogError(error);
+                    throw new ApplicationException(InternalError.InternalServerError, internalMessage: error);
+                }
+            }
+            catch (ApplicationException e) when (e.ErrorCode == InternalError.NotFound)
+            { 
+                logger.LogWarning($"Missing email template for {request.Channel} by {request.Tenant}");
             }
         }
     }
