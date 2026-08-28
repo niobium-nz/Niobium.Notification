c44c57c930ae1e44b67b5d831c1285544a18aa67
Wen Han <hcp5he11@gmail.com>
2026-08-28 23:25:19 +1200

    Added http based notification handler.

---
+++ src/Niobium.Notification.Host/Controllers/NotifyCommandHandler.cs
@@ -0,0 +1,62 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Niobium.Notification.Host.Controllers
+{
+    [ApiController]
+    [Route("notifycmd")]
+    public class NotifyCommandHandler(NotificationFlow flow, ILogger<NotifyCommandHandler> logger) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ConsumeAsync([FromBody] NotifyCommand message, CancellationToken cancellationToken)
+        {
+            Transform(message);
+            message.TryValidate(out ValidationState? validationState);
+            if (!validationState.IsValid)
+            {
+                logger.LogError($"Validation failed for order evt: {JsonMarshaller.Marshall(validationState.ToDictionary())}");
+                return this.BadRequest(validationState);
+            }
+
+            await flow.RunAsync(message, cancellationToken);
+            return this.NoContent();
+        }
+
+        private static void Transform(NotifyCommand evt)
+        {
+            // NotifyCommand.Parameters is a Dictionary<string, object>, there could be JsonElement values because of deserialization
+            // Transform them to string or IEnumerable<Dictionary<string, string>> for easier usage in templates
+            foreach (string? key in evt.Parameters.Keys.ToList())
+            {
+                if (evt.Parameters[key] is JsonElement jsonElement)
+                {
+                    switch (jsonElement.ValueKind)
+                    {
+                        case JsonValueKind.String:
+                            evt.Parameters[key] = jsonElement.GetString() ?? String.Empty;
+                            break;
+                        case JsonValueKind.Array:
+                            List<Dictionary<string, string>> list = [];
+                            foreach (JsonElement item in jsonElement.EnumerateArray())
+                            {
+                                if (item.ValueKind == JsonValueKind.Object)
+                                {
+                                    Dictionary<string, string> dict = [];
+                                    foreach (JsonProperty prop in item.EnumerateObject())
+                                    {
+                                        dict[prop.Name] = prop.Value.GetString() ?? String.Empty;
+                                    }
+                                    list.Add(dict);
+                                }
+                            }
+                            evt.Parameters[key] = list;
+                            break;
+                        default:
+                            evt.Parameters[key] = jsonElement.ToString() ?? String.Empty;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
